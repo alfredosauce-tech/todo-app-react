6dbea71484912d7f8be7ca8f2d76800bfbc46033
alfredosauce-tech <alfredo.aguadoo14@gmail.com>
2021-09-08 18:06:13 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    
+  super(props)
+  this.handleClick = this.handleClick.bind(this);
+this.state = {
+  Text:'',
+  todos:[],
+  isclicked: false
+};
+  }
+onChange = e => {
+  this.setState({
+    Text: e.target.value
+  })
 }
+//this is my onclickhandler button (handleclick)
+// //handleClick = ()=>{
+//   this.setState(prevState => ({
+//     isclicked: !prevState.isclicked
+//   }));
+// }
+//handleClick={index => this.delete(index)}
 
-export default App;
+  formsubmit= e => {
+
+    e.preventDefault()
+    this.setState({
+      todos : [...this.state.todos, this.state.Text],
+      Text: ''
+    })
+  }
+
+  delete = (index) => {
+    let objectCopy = [...this.state.todos]
+    objectCopy.splice(index, 1)
+    this.SetState({todos: [...objectCopy]})
+
+  }
+
+  
+  render() {
+    return (
+      <div>
+        <h1>
+
+        </h1>
+        <form onSubmit = {this.formsubmit}>
+        <input value = {this.state.text} onChange= {this.onChange}/>
+        <button>
+
+        {this.state.isclicked ? 'ON' : 'OFF'}
+        </button>
+    </form>
+    <div>
+    {this.state.todos.map((item , index)=>{
+    return <p key={index}>{this.state.todos[index]}
+    	<button onClick={() => props.handleClick(index)}>Delete</button>
+    
+      </p>
+    })}
+    </div>
+        
+      </div>
+    )
+  }
+}
+export default App
